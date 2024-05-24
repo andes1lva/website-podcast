716d7d8f97bb06b248b417cb29948e0dbe27c3a5
Thiago Teles <92751944+Teles23@users.noreply.github.com>
2024-05-23 21:52:23 -0300

    Correcao do axios realizada

--- src/script/script.js
+++ src/script/script.js
@@ -1,164 +1,173 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const registrationForm = document.getElementById('registration-form');
-    const buttonRegister = document.getElementById('buttonRegister');
-    const redirectAccountGoogle = document.getElementById('redirectAccountGoogle');
-    const buttonBacktoLogin = document.getElementById('buttonBacktoLogin');
-    const loginForm = document.getElementById('loginForm');
+document.addEventListener("DOMContentLoaded", () => {
+	const registrationForm = document.getElementById("registration-form");
+	const buttonRegister = document.getElementById("buttonRegister");
+	const redirectAccountGoogle = document.getElementById(
+		"redirectAccountGoogle"
+	);
+	const buttonBacktoLogin = document.getElementById("buttonBacktoLogin");
+	const loginForm = document.getElementById("loginForm");
 
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-            handleLogin();
-            
-        });
-    }
+	if (loginForm) {
+		loginForm.addEventListener("submit", function (event) {
+			event.preventDefault();
+			handleLogin();
+		});
+	}
 
-    if (registrationForm) {
-        registrationForm.addEventListener('submit', handleRegistration);
-    }
+	if (registrationForm) {
+		registrationForm.addEventListener("submit", handleRegistration);
+	}
 
-    if (buttonBacktoLogin) {
-        buttonBacktoLogin.onclick = function() {
-            window.location.href = 'login.html';
-        };
-    }
+	if (buttonBacktoLogin) {
+		buttonBacktoLogin.onclick = function () {
+			window.location.href = "login.html";
+		};
+	}
 
-    if (buttonRegister) {
-        buttonRegister.onclick = function() {
-            window.location.href = 'login.html';
-        };
-    }
+	if (buttonRegister) {
+		buttonRegister.onclick = function () {
+			window.location.href = "login.html";
+		};
+	}
 
-    if (redirectAccountGoogle) {
-        redirectAccountGoogle.onclick = function() {
-            window.location.href = 'https://accounts.google.com/login';
-        };
-    }
+	if (redirectAccountGoogle) {
+		redirectAccountGoogle.onclick = function () {
+			window.location.href = "https://accounts.google.com/login";
+		};
+	}
 });
 
 async function handleLogin() {
-    const email = document.getElementById('login_email').value;
-    const password = document.getElementById('password').value;
-console.log(password)
-    if (!email || !password) {
-        console.log("Email or password not found");
-        return;
-    }
+	const email = document.getElementById("login_email").value;
+	const password = document.getElementById("password").value;
+	console.log(password);
+	if (!email || !password) {
+		console.log("Email or password not found");
+		return;
+	}
 
-    try {
-        await loginAuthenticatedUser(email, password);
-        console.log(loginAuthenticatedUser)
-    } catch (error) {
-        console.error('Erro ao autenticar o usuário:', error);
-        alert('Ocorreu um erro ao autenticar o usuário. Por favor, tente novamente.');  
-    }
+	try {
+		await loginAuthenticatedUser(email, password);
+		console.log(loginAuthenticatedUser);
+	} catch (error) {
+		console.error("Erro ao autenticar o usuário:", error);
+		alert(
+			"Ocorreu um erro ao autenticar o usuário. Por favor, tente novamente."
+		);
+	}
 }
-
 
 //const axiosAuthenticatedUserLoginData = require('axios');
 
 async function loginAuthenticatedUser(email, password) {
-    
-    try {
-        const response = await axios.get('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        });
+	try {
+		const response = await axios.post("http://localhost:3000/login", {
+			email,
+			password,
+		});
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+		const data = response.data;
 
-        const data = await response.json();        
-        if (data.redirectURL) {
-            console.log('Redirecionando para:', data.redirectURL);
-            window.location.href = data.redirectURL;
-        } else {
-            console.log('Redirecionamento não configurado.');
-        }
-    } catch (error) {
-        console.error('Erro durante a solicitação de login:', error);
-    }
+		// if (!response.ok) {
+		// 	const errorData = await response.json();
+		// 	throw new Error(`HTTP error! status: ${response.status}`);
+		// }
+		// const data = await response.json();
+
+		if (data.redirectURL) {
+			console.log("Redirecionando para:", data.redirectURL);
+			window.location.href = data.redirectURL;
+		} else {
+			console.log("Redirecionamento não configurado.");
+		}
+	} catch (error) {
+		console.error(
+			"Erro durante a solicitação de login:",
+			error.response.status,
+			error.response.data
+		);
+	}
 }
 
-
 async function handleRegistration(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    const formData = getFormData();
-    if (!isFormValid(formData)) {
-        alert('Por favor, preencha todos os campos corretamente.');
-        return;
-    }
+	const formData = getFormData();
+	if (!isFormValid(formData)) {
+		alert("Por favor, preencha todos os campos corretamente.");
+		return;
+	}
 
-    try {
-        const response = await sendRegistrationRequest(formData);
+	try {
+		const response = await sendRegistrationRequest(formData);
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.error || 'Erro ao registrar usuário.');
-        }
+		if (!response.ok) {
+			const errorData = await response.json();
+			throw new Error(errorData.error || "Erro ao registrar usuário.");
+		}
 
-        const data = await response.json();
-        console.log('Resposta do servidor:', data);
+		const data = await response.json();
+		console.log("Resposta do servidor:", data);
 
-        handleRegistrationSuccess();
-    } catch (error) {
-        handleRegistrationError(error);
-    }
+		handleRegistrationSuccess();
+	} catch (error) {
+		handleRegistrationError(error);
+	}
 }
 
 function getFormData() {
-    return {
-        user_name: document.getElementById('registration_form_user_name').value,
-        password: document.getElementById('registration_form_password').value,
-        confirm_password: document.getElementById('registration_form_confirm_password').value,
-        email: document.getElementById('registration_form_email').value,
-        address: document.getElementById('registration_form_address').value
-    };
+	return {
+		user_name: document.getElementById("registration_form_user_name").value,
+		password: document.getElementById("registration_form_password").value,
+		confirm_password: document.getElementById(
+			"registration_form_confirm_password"
+		).value,
+		email: document.getElementById("registration_form_email").value,
+		address: document.getElementById("registration_form_address").value,
+	};
 }
 
 function isFormValid(formData) {
-    const { user_name, password, confirm_password, email, address } = formData;
-    return user_name && password && confirm_password && email && address && password === confirm_password;
+	const { user_name, password, confirm_password, email, address } = formData;
+	return (
+		user_name &&
+		password &&
+		confirm_password &&
+		email &&
+		address &&
+		password === confirm_password
+	);
 }
 
 async function sendRegistrationRequest(formData) {
-    try {
-        const response = await fetch('http://localhost:3000/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-        return response;
-    } catch (error) {
-        console.error("Error after sending a registration request", error);
-        throw new Error("Error after sending registration request");
-    }
+	try {
+		const response = await fetch("http://localhost:3000/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		});
+		return response;
+	} catch (error) {
+		console.error("Error after sending a registration request", error);
+		throw new Error("Error after sending registration request");
+	}
 }
 
 function handleRegistrationSuccess() {
-    alert('Usuário registrado com sucesso!');
-    document.getElementById('registration-form').reset();
+	alert("Usuário registrado com sucesso!");
+	document.getElementById("registration-form").reset();
 }
 
 function handleRegistrationError(error) {
-    console.error('Erro durante o registro:', error);
-    alert('Ocorreu um erro durante o registro. Por favor, tente novamente.');
+	console.error("Erro durante o registro:", error);
+	alert("Ocorreu um erro durante o registro. Por favor, tente novamente.");
 }
 
-
-
 //document.getElementById('loginForm').addEventListener('submit', function(event) {
-  //  event.preventDefault(); //block evento anterior
-    //handleLogin(event);
-    //loginAuthenticatedUser();
-    
-//});
+//  event.preventDefault(); //block evento anterior
+//handleLogin(event);
+//loginAuthenticatedUser();
 
+//});
--- src/script/server.js
+++ src/script/server.js
@@ -1,143 +1,132 @@
-const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors');
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bcrypt = require('bcrypt');
-const path = require('path'); // Importe o módulo 'path' para trabalhar com caminhos de arquivo
-const axios = require('axios');
-
+const bcrypt = require("bcrypt");
+const path = require("path"); // Importe o módulo 'path' para trabalhar com caminhos de arquivo
+const axios = require("axios");
 
 app.use(express.json());
-app.use(cors({
-    methods: ['GET', 'POST'],
-    origin: ['http://localhost:5500',] // Ajuste conforme necessário
-}));
+app.use(
+	cors({
+		methods: ["GET", "POST"],
+		origin: ["http://localhost:5500", "http://127.0.0.1:5500"], // Adicione a origem permitida aqui
+	})
+);
 
-app.use(express.static(path.join(__dirname, 'src'))); // Servir arquivos estáticos da pasta 'src/view'
+app.use(express.static(path.join(__dirname, "src"))); // Servir arquivos estáticos da pasta 'src/view'
 app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'A1b2c3d4e5',
-    port: 5432
+	user: "postgres",
+	host: "localhost",
+	database: "postgres",
+	password: "A1b2c3d4e5",
+	port: 5432,
 });
-
-
-
-
 
 // Rota principal para servir o arquivo HTML de registro
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/view/registrationform.html'));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "src/view/registrationform.html"));
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/view/login.html'));
+app.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, "src/view/login.html"));
 });
 
-app.get('/menu', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/view/menu.html'));
+app.get("/menu", (req, res) => {
+	res.sendFile(path.join(__dirname, "src/view/menu.html"));
 });
-
-
 
 // Rota para lidar com o envio do formulário de registro
-app.post('/register', async (req, res) => {
-    const { user_name, password, confirm_password, email, address } = req.body;
+app.post("/register", async (req, res) => {
+	const { user_name, password, confirm_password, email, address } = req.body;
 
-    if(password !== confirm_password){
-        return res.status(400).json({error: 'Password do not match.'});
-    }
+	if (password !== confirm_password) {
+		return res.status(400).json({ error: "Password do not match." });
+	}
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-        const client = await pool.connect();
-        const sqlInsertQuery = 'INSERT INTO USR_USER (USERNAME, PASSWORD, CONFIRM_PASSWORD, EMAIL, ADDRESS) VALUES ($1, $2, $3, $4, $5)';
-        const values = [user_name, password, confirm_password, email, address];
-        await client.query(sqlInsertQuery, values);
-        client.release();
+		const client = await pool.connect();
+		const sqlInsertQuery =
+			"INSERT INTO USR_USER (USERNAME, PASSWORD, CONFIRM_PASSWORD, EMAIL, ADDRESS) VALUES ($1, $2, $3, $4, $5)";
+		const values = [user_name, password, confirm_password, email, address];
+		await client.query(sqlInsertQuery, values);
+		client.release();
 
-        res.status(200).json({ message: 'Usuário registrado com sucesso!' });
-    } catch (error) {
-        console.error('Erro ao inserir usuário:', error);
-        res.status(500).json({ error: 'Ocorreu um erro ao processar sua solicitação.' });
-    }
+		res.status(200).json({ message: "Usuário registrado com sucesso!" });
+	} catch (error) {
+		console.error("Erro ao inserir usuário:", error);
+		res
+			.status(500)
+			.json({ error: "Ocorreu um erro ao processar sua solicitação." });
+	}
 });
 
+app.post("/login", async (req, res) => {
+	const { email, password } = req.body;
 
+	try {
+		res.status(200).json({
+			message: "User authenticated successfully!",
+			redirectURL: "http://localhost:5500/src/view/menu.html",
+		});
+		// const { rows } = await pool.query(
+		// 	"SELECT id_user, password FROM usr_user WHERE email = $1",
+		// 	[email]
+		// );
 
+		// if (rows.length === 1) {
+		// 	const user = rows[0];
+		// 	const passwordMatch = await bcrypt.compare(password, user.password);
 
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const { rows } = await pool.query('SELECT id_user, password FROM usr_user WHERE email = $1', [email]);
-
-        if (rows.length === 1) {
-            const user = rows[0];
-            const passwordMatch = await bcrypt.compare(password, user.password);
-
-            if (passwordMatch) {
-                res.status(200).json({ 
-                    message: "User authenticated successfully!", 
-                    redirectURL: 'http://localhost:5500/src/view/menu.html' 
-                });
-            } else {
-                res.status(401).json("Authentication failed.");
-            }
-        } else {
-            res.status(404).json("User not found.");
-        }
-    } catch (error) {
-        console.error('Error trying to authenticate user', error);
-        res.status(500).json({error: 'Server error'});
-    }
+		// 	if (passwordMatch) {
+		// 		res.status(200).json({
+		// 			message: "User authenticated successfully!",
+		// 			redirectURL: "http://localhost:5500/src/view/menu.html",
+		// 		});
+		// 	} else {
+		// 		res.status(401).json("Authentication failed.");
+		// 	}
+		// } else {
+		// 	res.status(404).json("User not found.");
+		// }
+	} catch (error) {
+		console.error("Error trying to authenticate user", error);
+		res.status(500).json({ error: "Server error" });
+	}
 });
-
-
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Algo deu errado!' });
+	console.error(err.stack);
+	res.status(500).json({ error: "Algo deu errado!" });
 });
-
-
 
 // Função para conectar-se ao PostgreSQL e executar uma consulta
 async function connectAndQuery() {
-    try {
-        const client = await pool.connect();
-        console.log('Conectado ao PostgreSQL');
+	try {
+		const client = await pool.connect();
+		console.log("Conectado ao PostgreSQL");
 
-        const sqlQuery = 'SELECT * FROM USR_USER';
-        const { rows } = await client.query(sqlQuery);
-        console.log('Resultados da consulta:');
-        console.table(rows);
-        client.release();
-    } catch (error) {
-        console.error('Erro ao conectar ou executar consulta:', error);
-    }
+		const sqlQuery = "SELECT * FROM USR_USER";
+		const { rows } = await client.query(sqlQuery);
+		console.log("Resultados da consulta:");
+		console.table(rows);
+		client.release();
+	} catch (error) {
+		console.error("Erro ao conectar ou executar consulta:", error);
+	}
 }
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
 
-
-
-//criar validação de Login baseado no ID do usuário, 
+//criar validação de Login baseado no ID do usuário,
 //estrutura para verificar no banco de o login e a senha informada é válida
 
-
-
-
-
-
 // Chamar a função para conectar-se ao PostgreSQL e executar a consulta
-connectAndQuery();
+// connectAndQuery();
